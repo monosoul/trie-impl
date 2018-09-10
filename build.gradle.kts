2e26c7bce141b61162d9f71f0068090a8adb6168
Неведомский Андрей Андреевич <AANevedomsky.SBT@sberbank.ru>
2018-09-10 17:18:05 +0500

    add gradle

---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+group = "com.github.monosoul.trie"
+version = "1.0"
+
+plugins {
+    java
+}
+
+apply {
+    plugin("org.junit.platform.gradle.plugin")
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+    testCompile("org.junit.jupiter:junit-jupiter-api:5.1.0")
+    testCompile("org.junit.jupiter:junit-jupiter-params:5.1.0")
+    testRuntime("org.junit.jupiter:junit-jupiter-engine:5.1.0")
+    testCompile("org.assertj:assertj-core:3.9.1")
+    testCompile("org.projectlombok:lombok:1.16.20")
+    testCompile("com.google.guava:guava:24.1-jre")
+}
+
+repositories {
+    jcenter()
+}
+
+buildscript {
+    repositories{
+        jcenter()
+    }
+
+    dependencies{
+        classpath("org.junit.platform:junit-platform-gradle-plugin:1.1.0")
+    }
+}
